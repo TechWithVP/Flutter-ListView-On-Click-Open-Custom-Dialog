c2d90fa7520a94abbaffbf963a5a0225bf563de4
Vivek <vickystudy0910@gmail.com>
2020-07-10 11:14:31 +0530

    Final Commit

---
+++ android/app/src/main/kotlin/com/example/ListOnClick/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ListOnClick
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+
+// Main Function Start
+void main() {
+  runApp(MyApp());
+}
+// Main Function Ends
+
+// First Stateless Widget with MaterialApp Start
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ListViewPage(),
+    );
+  }
+}
+// First Stateless Widget with MaterialApp Ends
+
+
+// Main Stateful Widget Start
+class ListViewPage extends StatefulWidget {
+  @override
+  _ListViewPageState createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  // Title List Here
+  var titleList = [
+    "Success",
+    "Motivation",
+    "Hard Work",
+    "Decision",
+    "Confidence",
+    "Business",
+    "Team Work"
+  ];
+
+  // Description List Here
+  var descList = [
+    "Push yourself, because no one else is going to do it for you.",
+    "Your limitation—it's only your imagination.",
+    "Hard Work changes the life.",
+    "Sometimes it's the smallest decisions that can change your life forever.",
+    "Confidence is the most beautiful thing you can possess",
+    "A big business starts small.",
+    "Talent wins games, but teamwork and intelligence win championships."
+  ];
+
+  // Image Name List Here
+  var imgList = [
+    "assets/images/img1.jpg",
+    "assets/images/img2.jpg",
+    "assets/images/img3.jpg",
+    "assets/images/img4.jpg",
+    "assets/images/img5.jpg",
+    "assets/images/img6.jpg",
+    "assets/images/img7.jpg"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // MediaQuery to get Device Width
+    double width = MediaQuery.of(context).size.width * 0.6;
+    return Scaffold(
+      appBar: AppBar(
+        // App Bar
+        title: Text(
+          "ListView On-Click Event",
+          style: TextStyle(color: Colors.grey),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      // Main List View With Builder
+      body: ListView.builder(
+        itemCount: imgList.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              // This Will Call When User Click On ListView Item
+              showDialogFunc(context, imgList[index], titleList[index], descList[index]);
+            },
+            // Card Which Holds Layout Of ListView Item
+            child: Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    child: Image.asset(imgList[index]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          titleList[index],
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: width,
+                          child: Text(
+                            descList[index],
+                            maxLines: 3,
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey[500]),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// This is a block of Model Dialog 
+showDialogFunc(context, img, title, desc) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return Center(
+        child: Material(
+          type: MaterialType.transparency,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+            ),
+            padding: EdgeInsets.all(15),
+            height: 320,
+            width: MediaQuery.of(context).size.width * 0.7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image.asset(
+                    img,
+                    width: 200,
+                    height: 200,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  // width: 200,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      desc,
+                      maxLines: 3,
+                      style: TextStyle(fontSize: 15, color: Colors.grey[500]),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}
